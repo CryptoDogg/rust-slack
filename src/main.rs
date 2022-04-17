61631e14411eb1a1db12d092714fcf23355c0b14
CryptoDogg <d.enkikairo@gmail.com>
2022-04-17 18:37:52 +0900

    test github actions post

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,31 @@ use dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let slack_webhook = dotenv::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL must be set");
-    let response_body = reqwest::Client::new()
-        .post(slack_webhook)
+    let echo_json: serde_json::Value = reqwest::Client::new()
+        .post("https://jsonplaceholder.typicode.com/posts")
         .json(&serde_json::json!({
-            "text": "Hello, World!",
+            "title": "Reqwest.rs",
+            "body": "https://docs.rs/reqwest",
+            "userId": 1
         }))
         .send()
         .await?
-        .text()
+        .json()
         .await?;
 
-    println!("response body {:#?}", response_body);
+    println!("{:#?}", echo_json);
+    // let slack_webhook = dotenv::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL must be set");
+    // let response_body = reqwest::Client::new()
+    //     .post(slack_webhook)
+    //     .json(&serde_json::json!({
+    //         "text": "Hello, World!",
+    //     }))
+    //     .send()
+    //     .await?
+    //     .text()
+    //     .await?;
+
+    // println!("response body {:#?}", response_body);
     // Object(
     //     {
     //         "body": String(
